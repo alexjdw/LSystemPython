6b30770273c6f2dffbb3e8199d59be2220005c91
awildtechno <ajdweavers@gmail.com>
2019-04-26 19:01:25 -0700

    Update LSystemPygame.py
    Cleaned up my code from reddit. Major changes:
    
    @Debug allows step-by-step drawing if DEBUG == true.
    
    LSystem.draw_line() has been updated to calculate the new position.
    LSystem.rotate() has been updated to remove the above calculation as it is never needed after the change.

--- LSystemPygame.py
+++ LSystemPygame.py
@@ -1,38 +1,52 @@
-import math
 import numpy as np
 import pygame
 import os
 
+DEBUG = False
+
+
+# Draws and waits after execution of a function if DEBUG is true. Otherwise no change.
+def Debug(function):
+    if DEBUG == True:
+        def wrapper(*args, **kwargs):
+            function(*args, **kwargs)
+            pygame.display.update()
+            pygame.time.delay(100)
+
+        return wrapper
+    return function
+
 
 class LSystem:
-    rotation, char_pos, theta, width = 0, 0, 0, 1
-    stack = []
-    next_pos = cur_pos = np.zeros(2, float)
-
     def __init__(self, max_iteration, side_length, theta,
                  axiom, instructions, starting_info):
+        self.rotation, self.char_pos, self.theta, self.width = 0, 0, 0, 1
+        self.stack = []
+
         self.max_iteration = max_iteration
         self.side_length = side_length
-        self.theta = theta * math.pi / 180
+        self.theta = theta * np.pi / 180
         self.next_string = axiom
         self.instructions = instructions
         self.starting_info = starting_info
         self.starting_pos = np.array([starting_info[0] * screen.get_width(),
-                                      starting_info[1] * screen.get_height()])
+                                    starting_info[1] * screen.get_height()])
         self.reset()
-        # Basically a switch statement
-        self.switch = {
+
+        # A dictionary of possible operations the fractal can take.
+        self.ops = {
             **dict.fromkeys(('F', 'G'), lambda x: x.draw_line()),
             '>': lambda x: x.rotate(-x.theta),
             '<': lambda x: x.rotate(x.theta),
             '[': lambda x: x.save_pos(),
             ']': lambda x: x.restore_pos(),
-        }
+            }
 
     # Reset position and angle to original position to draw
     def reset(self):
         self.rotation = self.starting_info[2]
-        self.cur_pos = self.next_pos = self.starting_pos
+        self.next_pos = np.copy(self.starting_pos)
+        self.cur_pos = np.copy(self.starting_pos)
 
     # Grow the L-system string max_iteration times
     def grow(self):
@@ -45,20 +59,23 @@ class LSystem:
     # Draw a fractal based on the string
     def interpret(self):
         for self.char_pos, char in enumerate(self.next_string):
-            if char in self.switch:
-                self.switch[char](self)
+            if char in self.ops:
+                self.ops[char](self)
 
     # Change the angle of rotation and calculate height and width of next line to draw
     def rotate(self, theta):
         self.rotation += theta
-        x = self.side_length * math.cos(self.rotation)
-        y = self.side_length * math.sin(self.rotation)
-        self.next_pos = (self.cur_pos + [x, y])
 
     # Draw a line using rgb, starting at cur_pos and ending at next_pos
+    @Debug
     def draw_line(self):
+        x = self.side_length * np.cos(self.rotation)
+        y = self.side_length * np.sin(self.rotation)
+
+        self.next_pos = (self.cur_pos + [x, y])
+
         r, g, b = (self.char_pos % 255, (len(self.next_string) - self.char_pos) % 255,
-                   self.char_pos * 255 // len(self.next_string))
+                self.char_pos * 255 // len(self.next_string))
         pygame.draw.line(screen, (r, g, b), self.cur_pos, self.next_pos, self.width)
         self.cur_pos = self.next_pos
 
@@ -84,8 +101,9 @@ if __name__ == '__main__':
                 LSystem(10, 15, 45, 'F', {'F': 'G[<F]>F', 'G': 'GG'}, (1/2, 1/2, 45)),           # fractal tree
                 LSystem(5, 20, 25, 'X', {'X': 'F>[[X]<X]<F[<FX]>X', 'F': 'FF'}, (1/2, 1/2, 25)),     # fractal plant
                 LSystem(4, 8, 72, 'F<F<F<F<F', {'F': 'F<F>>F>F<F<F'}, (1/2, 1/2, 72)),         # hexagon thingy
-                LSystem(3, 5, 90, 'F', {'F': 'F>F<F<F<G>F>F>F<F', 'G': 'GGG'}, (1/2, 1/2, 90))     # square thingy
+                LSystem(4, 4, 90, 'F', {'F': 'F>F<F<F<G>F>F>F<F', 'G': 'GGG'}, (1/2, 1/2, 90))     # square thingy
                 ]
+
     # Grow and Interpret each LSystem
     for i in range(len(LSystems)):
         LSystems[i].grow()
